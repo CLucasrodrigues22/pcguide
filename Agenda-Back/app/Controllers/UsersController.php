9f9093af4bdae4676236e813561c65885e9bc905
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-07-28 22:03:00 -0300

    refactor: method upload file

--- Agenda-Back/app/Controllers/UsersController.php
+++ Agenda-Back/app/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controllers;
 
-// recursos estáticos
+// static resources
 use MVC\Controller\Action;
 use MVC\Model\Container;
 
@@ -30,44 +30,13 @@ class UsersController extends Action
     {
         try {
             $user = Container::getModel('Users');
-            // storage user photo
-            $upload['directory'] = 'storage/usersPhoto/';
-            $upload['extensions'] = ['png', 'jpg', 'jpeg'];
-
-            $upload['erros'][0] = 'Não houve erro';
-            $upload['erros'][1] = 'O arquivo no upload é maior do que o limite do PHP';
-            $upload['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
-            $upload['erros'][3] = 'O upload do arquivo foi feito parcialmente';
-            $upload['erros'][4] = 'Não foi feito o upload do arquivo';
-
-            if ($_FILES['photo']['error'] === 0) {
-
-                // Dividindo o nome do nome da imagem (imagem . extensão)
-                $photo = explode('.', $_FILES['photo']['name']);
-                // Pegando a extensão da imagem
-                $extension = strtolower(end($photo));
-                // Validando Extensão
-                if (array_search($extension, $upload['extensions']) === false) { // percorre array de $upload
-                    // se tiver erro >
-                    $feedback = 'Só é aceito imagem com extensão png';
-                    header("Location: /createusers?feedback=$feedback");
-                    exit;
-                }
-                // nome para ser salvo no banco de dados
-                $namePhoto = md5($photo[0]) . '-' . date('YmdHmi') . '.' . $extension;
-
-                // Verifica se é possível mover o arquivo para a pasta escolhida
-                if (move_uploaded_file($_FILES['photo']['tmp_name'], $upload['directory'] . $namePhoto)) {
-                    $user->__set('photo', $namePhoto);
-                } else {
-                    echo 'Ocorreu o seguinte erro' . $upload['erros'][$_FILES['photo']['error']];
-                }
-            }
+            $storagePhoto = $this->storage($_FILES['photo'], 'storage/usersPhoto/');
             // Send user data received from form to model
             $user->__set('name', $_POST['name']);
             $user->__set('email', $_POST['email']);
             $user->__set('password', password_hash($_POST['password'], PASSWORD_DEFAULT));
             $user->__set('phone', $_POST['phone']);
+            $user->__set('photo', $storagePhoto);
             $response = $user->create();
             http_response_code(201);
             echo json_encode($response);
--- Agenda-Back/vendor/MVC/Controller/Action.php
+++ Agenda-Back/vendor/MVC/Controller/Action.php
@@ -2,7 +2,11 @@
 
 namespace MVC\Controller;
 
-abstract class Action {
+use ArrayObject;
+use Cake\Database\Type\StringType;
+
+abstract class Action
+{
     protected $view;
 
     public function __construct()
@@ -16,16 +20,46 @@ abstract class Action {
         $this->view->page = $view;
 
         //validando se o layout existe
-        if(file_exists("../resources/layouts/".$layout.".phtml")) {
-            require_once "../resources/layouts/".$layout.".phtml";
+        if (file_exists("../resources/layouts/" . $layout . ".phtml")) {
+            require_once "../resources/layouts/" . $layout . ".phtml";
         } else {
             echo 'Layout não encontrado';
         }
     }
-    
+
     // localiza e renderinza a view passada por parametro no controller
-    protected function content() 
+    protected function content()
     {   // $this->view('dir/view');
-        require_once "../resources/views/".$this->view->page.".phtml";
+        require_once "../resources/views/" . $this->view->page . ".phtml";
+    }
+
+    protected function storage($filedata, $path, $extensions = ['png', 'jpg', 'jpeg', 'pdf'])
+    {
+
+        try {
+            $file = explode('.', $filedata['name']);
+
+            // get extension
+            $extension = strtolower(end($file));
+
+            if (array_search($extension, $extensions) === false) {
+                $feedback = 'Extension not allowed';
+                return $feedback;
+            }
+            // nome para ser salvo no banco de dados
+            $nameFile = $path . md5($file[0]) . '-' . date('YmdHmi') . '.' . $extension;
+            // // Verifica se é possível mover o arquivo para a pasta escolhida
+            if (move_uploaded_file($filedata['tmp_name'], $nameFile)) {
+                return $nameFile;
+            } else {
+                return $filedata["error"];
+            }
+        } catch (\Throwable $e) {
+            $response = [
+                "erro" => true,
+                "messagem" => "Error: " . $e
+            ];
+            return $response;
+        }
     }
 }
