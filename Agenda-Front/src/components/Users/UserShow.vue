0bdf9083106af2be6363ba30ef54612ce7c12f64
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-07-29 17:20:39 -0300

    feat: add user's components

---
+++ Agenda-Front/src/components/Users/UserShow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Component UsersShow</h1>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: 'UsersShow'
+}
+</script>
---
+++ Agenda-Front/src/components/Users/UsersCreate.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Component UsersCreate</h1>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: 'UsersCreate'
+}
+</script>
