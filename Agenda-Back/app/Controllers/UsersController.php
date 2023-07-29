9cf62a2f8b81041172a11acdbe8758910653c527
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-07-28 22:10:49 -0300

    refactor: update photo in user's update method

--- Agenda-Back/app/Controllers/UsersController.php
+++ Agenda-Back/app/Controllers/UsersController.php
@@ -73,47 +73,22 @@ class UsersController extends Action
         try {
             $id = $_GET['id'];
             $user = Container::getModel('Users');
-            $upload['directory'] = 'storage/usersPhoto/';
-            $upload['extensions'] = ['png', 'jpg'];
 
-            $upload['erros'][0] = 'Não houve erro';
-            $upload['erros'][1] = 'O arquivo no upload é maior do que o limite do PHP';
-            $upload['erros'][2] = 'O arquivo ultrapassa o limite de tamanho especifiado no HTML';
-            $upload['erros'][3] = 'O upload do arquivo foi feito parcialmente';
-
-
-            if ($_FILES['photo']['error'] === 0 && $_FILES['photo']['name'] != '') {
-
-                // Remove current photo
-                $photoOld = $user->showOnlyUser($id);
-                $path = $upload['directory'] . $photoOld->photo;
-                unlink($path);
-
-                $photo = explode('.', $_FILES['photo']['name']);
-
-                // get photo extension
-                $extension = strtolower(end($photo));
-
-                // validate extension
-                if (array_search($extension, $upload['extensions']) === false) { // percorre array de $upload
-                    // if has erro >
-                    $feedback = 'Só é aceito imagem com extensão png';
-                    header("Location: /users?feedback=$feedback");
-                    exit;
-                }
-
-                // photo's name with md5 e date
-                $namePhoto = md5($photo[0]) . '-' . date('YmdHmi') . '.' . $extension;
-
-                // validating and moving file to directory
-                if (move_uploaded_file($_FILES['photo']['tmp_name'], $upload['directory'] . $namePhoto)) {
-                    $user->__set('photo', $namePhoto);
-                }
+            if ($_FILES['photo']['name'] != '') {
+                // setting attribut photo with new file name
+                $storagePhoto = $this->storage($_FILES['photo'], 'storage/usersPhoto/');
+                $user->__set('photo', $storagePhoto);
             } else {
                 //recovery current photo's name if $_FILES if empty
                 $photoOld = $user->showOnlyUser($id);
                 $user->__set('photo', $photoOld->photo);
             }
+
+            // else {
+            //     //recovery current photo's name if $_FILES if empty
+            //     $photoOld = $user->showOnlyUser($id);
+            //     $user->__set('photo', $photoOld->photo);
+            // }
 
             $user->__set('name', $_POST['name']);
             $user->__set('email', $_POST['email']);
