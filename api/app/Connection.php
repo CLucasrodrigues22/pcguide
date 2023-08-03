fade16a264597a927e0fbe2d63da6b87285862fc
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-08-02 22:36:16 -0300

    refactor: refactoring structure past

---
+++ api/app/Connection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+class Connection
+{
+
+    public static function getDb()
+    {
+        try {
+            $conn = new \PDO(
+                "mysql:host=localhost;dbname=pcguidedev;charset=utf8",
+                "root",
+                "password"
+            );
+
+            return $conn;
+        } catch (\PDOException $e) {
+            echo 'Erro' . $e;
+        }
+    }
+}
